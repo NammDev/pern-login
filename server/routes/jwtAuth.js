5e5d768a09c94110be74c5586b1b5141ef55a976
Nam <namdeveloper.ca@gmail.com>
2023-02-06 22:07:24 +0700

    Build Login Route

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -11,8 +11,7 @@ router.post('/register', async (req, res) => {
     // 2. Check if user exist (if true throw error)
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email])
     if (user.rows.length !== 0) {
-      console.log(user.rows.length)
-      return res.status(401).send('User already exist')
+      return res.status(401).json('User already exist')
     }
 
     // 3. Bcrypt the user password
@@ -29,6 +28,32 @@ router.post('/register', async (req, res) => {
     const token = jwtGenerator(newUser.rows[0].user_id)
 
     // 6. Send respond
+    res.json({ accessToken: token })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    // 1. Destructure the req.body (email, password)
+    const { email, password } = req.body
+
+    // 2. Check if user exist (if false throw error)
+    const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email])
+    if (user.rows.length === 0) {
+      return res.status(401).json('Email is incorrect!')
+    }
+
+    // 3. Check if input password is same as database password
+    const validPassword = await bcrypt.compare(password, user.rows[0].user_password)
+    if (!validPassword) return res.status(401).json('Password is incorrect')
+
+    // 4. Give them jwt token
+    const token = jwtGenerator(user.rows[0].user_id)
+
+    // 5. Send respond
     res.json({ accessToken: token })
   } catch (error) {
     console.log(error)
