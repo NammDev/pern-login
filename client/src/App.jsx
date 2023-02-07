9819ed490b35d5e2ed195d9f8b7ce00d966f8aa5
Nam <namdeveloper.ca@gmail.com>
2023-02-07 10:59:23 +0700

    Add Style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from './components/Dashboard'
 import PublicRoute from './routes/PublicRoute'
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <Router>
-      <div className='container'>
+      <main className='App'>
         <Routes>
           <Route
             path='/login'
@@ -59,7 +59,7 @@ function App() {
             }
           />
         </Routes>
-      </div>
+      </main>
       <ToastContainer />
     </Router>
   )
