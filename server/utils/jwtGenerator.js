a943115fe530b4bdd788224d2c59e33f9f937b6d
Nam <namdeveloper.ca@gmail.com>
2023-02-07 08:12:18 +0700

    JWT middleware

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 
 function jwtGenerator(id) {
   const payload = {
-    user: id,
+    user: {
+      id,
+    },
   }
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2hr' })
 }
